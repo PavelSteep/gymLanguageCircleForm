3f28239559e6bc191a18b8d2dbe5405c1da98d3a
PavelSteep <bpv53@ukr.net>
2023-10-29 15:16:36 +0100

    Deploy gymLanguageCircleForm website

--- server/server.js
+++ server/server.js
@@ -36,6 +36,6 @@ app.post('/send-email', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
 });
